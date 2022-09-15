422c0f38324633146c368202147369ec11851e98
Letícia Costa Mancini <leticia.mancini@ibm.com>
2022-09-15 11:45:21 -0300

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+// Substitui o const express = require('express')
+const app = express();
+
+app.get("/ads", (request, response) => {
+  return response.json([
+    { id: 1, name: "Anúncio 1" },
+    { id: 2, name: "Anúncio 2" },
+    { id: 3, name: "Anúncio 3" },
+    { id: 4, name: "Anúncio 4" },
+  ]);
+});
+
+// Porta de acesso
+app.listen(3333);
