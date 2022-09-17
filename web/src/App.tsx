dd367e0f6238a57a98eab4f9692179121f139c4e
Letícia Costa Mancini <leticia.mancini@ibm.com>
2022-09-17 18:00:38 -0300

    feat(webapp): modal

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,12 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { GameController } from "phosphor-react";
 import { useEffect, useState } from "react";
+import logoImg from "./assets/logo-nlw-esports.svg";
 
 import { CreateAdBanner } from "./components/CreateAdBanner";
 import { GameBanner } from "./components/GameBanner";
 
 import "./styles/main.css";
-
-import logoImg from "./assets/logo-nlw-esports.svg";
 
 interface Game {
   id: string;
@@ -50,7 +51,89 @@ function App() {
         })}
       </div>
 
-      <CreateAdBanner />
+      <Dialog.Root>
+        <CreateAdBanner />
+
+        <Dialog.Portal>
+          <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
+
+          <Dialog.Content className="bg-[#2a2634] fixed py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25">
+            <Dialog.Title className="text-3xl font-black">
+              Publique um anúncio
+            </Dialog.Title>
+
+            <Dialog.Content>
+              <form action="">
+                <div>
+                  <label htmlFor="game">Qual o game?</label>
+                  <input
+                    id="game"
+                    type="text"
+                    placeholder="Selecione o game que deseja jogar"
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor="name">Seu nome (ou nickname)</label>
+                  <input
+                    id="name"
+                    type="text"
+                    placeholder="Como te chamam dentro do game?"
+                  />
+                </div>
+
+                <div>
+                  <div>
+                    <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
+                    <input
+                      id="yearsPlaying"
+                      type="number"
+                      placeholder="Tudo bem ser ZERO"
+                    />
+                  </div>
+
+                  <div>
+                    <label htmlFor="discord">Qual seu Discord?</label>
+                    <input
+                      id="discord"
+                      type="text"
+                      placeholder="Usuario#0000"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <div>
+                    <label htmlFor="weekDays">Quando costuma jogar?</label>
+                  </div>
+
+                  <div>
+                    <label htmlFor="hourStart">Qual horário do dia?</label>
+
+                    <div>
+                      <input id="hourStart" type="time" placeholder="De" />
+                      <input id="hourEnd" type="time" placeholder="Até" />
+                    </div>
+                  </div>
+                </div>
+
+                <div>
+                  <input type="checkbox" />
+                  Costumo me conectar ao chat de voz
+                </div>
+
+                <footer>
+                  <button>Cancelar</button>
+                  <button type="submit">
+                    <GameController />
+                    Encontrar Duo
+                  </button>
+                </footer>
+              </form>
+            </Dialog.Content>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
   );
 }
