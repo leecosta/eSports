48bea28a5a0fde22d44d6be1c781ce752787717d
Letícia Costa Mancini <leticia.mancini@ibm.com>
2022-09-17 23:45:38 -0300

    feat(mobile): conexao back

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -1,7 +1,6 @@
 import { LinearGradient } from "expo-linear-gradient";
 import {
   ImageBackground,
-  ImageSourcePropType,
   Text,
   TouchableOpacity,
   TouchableOpacityProps,
@@ -13,9 +12,9 @@ import { styles } from "./styles";
 
 export interface GameCardProps {
   id: string;
-  name: string;
-  ads: string;
-  cover: ImageSourcePropType;
+  title: string;
+  _count: { ads: number };
+  bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps {
@@ -26,10 +25,10 @@ export function GameCard({ data, ...rest }: Props) {
   //TouchableOpacity torna a região clicável
   return (
     <TouchableOpacity style={styles.container} {...rest}>
-      <ImageBackground style={styles.cover} source={data.cover}>
+      <ImageBackground style={styles.cover} source={{ uri: data.bannerUrl }}>
         <LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
-          <Text style={styles.name}>{data.name}</Text>
-          <Text style={styles.ads}>{data.ads} anúncios</Text>
+          <Text style={styles.name}>{data.title}</Text>
+          <Text style={styles.ads}>{data._count.ads} anúncios</Text>
         </LinearGradient>
       </ImageBackground>
     </TouchableOpacity>
--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,13 +1,23 @@
-import { View, Image, FlatList } from "react-native";
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { FlatList, Image, View } from "react-native";
 import logoImg from "../../assets/logo-nlw-esports.png";
-import { GameCard } from "../../components/GameCard";
+
+import { GameCard, GameCardProps } from "../../components/GameCard";
 import { Heading } from "../../components/Heading";
-import { GAMES } from "../../utils/games";
 
 import { styles } from "./styles";
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("http://192.168.0.17:3333/games")
+      .then((data) => setGames(data.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image source={logoImg} style={styles.logo} />
@@ -19,7 +29,7 @@ export function Home() {
 
       {/* Para lista */}
       <FlatList
-        data={GAMES}
+        data={games}
         keyExtractor={(item) => item.id} //key
         renderItem={({ item }) => <GameCard data={item} />} //O que quer renderizar
         showsHorizontalScrollIndicator={false}
