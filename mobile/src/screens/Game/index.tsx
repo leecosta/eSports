e98b83e0a03023fbdfdbbd25413f77f8623d194b
Letícia Costa Mancini <leticia.mancini@ibm.com>
2022-09-18 23:46:35 -0300

    feat(mobile): screen game

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,18 +1,79 @@
-import { useRoute } from "@react-navigation/native";
+import { Entypo } from "@expo/vector-icons";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { GameParams } from "../../@types/navigation";
+import logoImg from "../../assets/logo-nlw-esports.png";
 
 import { Background } from "../../components/Background";
+import { DuoCard, DuoCardProps } from "../../components/DuoCard";
+import { Heading } from "../../components/Heading";
 
+import { THEME } from "../../theme";
 import { styles } from "./styles";
 
 export function Game() {
+  const [duos, setDuos] = useState<DuoCardProps[]>([]);
+
+  const navigation = useNavigation();
   const route = useRoute();
   const game = route.params as GameParams;
 
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
+  useEffect(() => {
+    axios
+      .get(`http://192.168.0.17:3333/games/${game.id}/ads`)
+      .then((data) => setDuos(data.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <Background>
-      <SafeAreaView style={styles.container}></SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={handleGoBack}>
+            <Entypo
+              name="chevron-thin-left"
+              color={THEME.COLORS.CAPTION_300}
+              size={20}
+            />
+          </TouchableOpacity>
+
+          <Image source={logoImg} style={styles.logo} />
+
+          {/* Macete para centralizar a imagem */}
+          <View style={styles.right} />
+        </View>
+
+        <Image
+          source={{ uri: game.bannerUrl }}
+          style={styles.cover}
+          resizeMode="cover"
+        />
+
+        <Heading title={game.title} subtitle="Conecte-se e comece a jogar!" />
+
+        <FlatList
+          data={duos}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <DuoCard data={item} onConnect={() => {}} />
+          )}
+          horizontal
+          style={styles.containerList}
+          contentContainerStyle={styles.containerList} //Conteúdo da lista
+          showsHorizontalScrollIndicator={false}
+          ListEmptyComponent={() => (
+            <Text style={styles.emptyListText}>
+              Não há anúncios publicados ainda.
+            </Text>
+          )}
+        />
+      </SafeAreaView>
     </Background>
   );
 }
--- mobile/src/screens/Game/styles.ts
+++ mobile/src/screens/Game/styles.ts
@@ -1,5 +1,42 @@
+import { THEME } from "./../../theme/index";
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  header: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 32,
+    marginTop: 28,
+    justifyContent: "space-between",
+  },
+  logo: {
+    width: 72,
+    height: 40,
+  },
+  // Colocar mesma medida do icone, para a logo ficar bem centralizada
+  right: {
+    width: 20,
+    height: 20,
+  },
+  cover: {
+    width: 311,
+    height: 160,
+    borderRadius: 8,
+    marginTop: 32,
+  },
+  containerList: {
+    paddingLeft: 32,
+    paddingRight: 64,
+    alignItems: "flex-start",
+  },
+  emptyListText: {
+    color: THEME.COLORS.CAPTION_300,
+    fontSize: THEME.FONT_SIZE.SM,
+    fontFamily: THEME.FONT_FAMILY.REGULAR,
+  },
 });
